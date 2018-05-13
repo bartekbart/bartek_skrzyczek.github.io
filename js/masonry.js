293316ea224af91ae5f752dabb415b21f186e025
bartek-skrzyczek <bartek.skrzyczek@gmail.com>
2018-05-13 13:14:26 +0200

    initial commit

--- js/masonry.js
+++ js/masonry.js
@@ -4,18 +4,27 @@ window.onload = function() {
   window.addEventListener('resize', go);
 
   function go(){
-    let entireSpace = document.getElementsByClassName("featured-projects-block__item")[0].offsetHeight;
-    let cardHeight = document.getElementsByClassName("content")[0].offsetHeight;
+    let entireSpace = document.getElementsByClassName("featured-projects-block__item")[1].offsetHeight;
+    let cardHeight = document.getElementsByClassName("content")[1].offsetHeight;
+    let margin = entireSpace - cardHeight;
+    let container = document.getElementsByClassName("featured-projects-block__item")[3];
+    let container2 = document.getElementsByClassName("featured-projects-block__item")[4];
 
-    var margin = entireSpace - cardHeight;
-    let container = document.getElementsByClassName("featured-projects-block__item")[2];
+    let card0 = document.getElementsByClassName("content")[0].offsetHeight;
+    let card1 = document.getElementsByClassName("content")[1].offsetHeight;
+    let card2 = document.getElementsByClassName("content")[2].offsetHeight;
+    let card3 = document.getElementsByClassName("content")[3].offsetHeight;
+    let card4 = document.getElementsByClassName("content")[4].offsetHeight;
 
+    let leftColumnHeight = card0 + card2;
+    let RightColumnHeight = card1 + card3;
+    let mT = leftColumnHeight - RightColumnHeight;
+    console.log(RightColumnHeight);
+    console.log(leftColumnHeight);
+    console.log(mT);
     container.style.marginTop = ( - margin + "px" );
+    container2.style.marginTop = ( - (leftColumnHeight - RightColumnHeight) - 30 + "px" );
   };
-
-
-
-
 
   var addEvent = function(object, type, callback) {
       if (object == null || typeof(object) == 'undefined') return;
@@ -31,10 +40,10 @@ window.onload = function() {
   addEvent(window, "resize", function(event) {
     console.log('resized');
 
-    let entireSpace = document.getElementsByClassName("featured-projects-block__item")[0].offsetHeight;
-    let cardHeight = document.getElementsByClassName("content")[0].offsetHeight;
-    var margin = entireSpace - cardHeight;
-    let container = document.getElementsByClassName("featured-projects-block__item")[2];
+    let entireSpace = document.getElementsByClassName("featured-projects-block__item")[1].offsetHeight;
+    let cardHeight = document.getElementsByClassName("content")[1].offsetHeight;
+    let margin = entireSpace - cardHeight;
+    let container = document.getElementsByClassName("featured-projects-block__item")[3];
 
     container.style.marginTop = ( - margin + "px" );
 
